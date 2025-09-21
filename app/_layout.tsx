c00fa0a5c6725920508c73d6a679e449b396a771
Zikria11 <zikriaakhtar08@gmail.com>
2025-09-21 16:16:26 +0500

    first commit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -10,8 +10,6 @@ export const unstable_settings = {
 };
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
